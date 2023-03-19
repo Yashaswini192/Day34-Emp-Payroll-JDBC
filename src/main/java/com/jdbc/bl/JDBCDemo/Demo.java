f9a222faf5f81694d64775155f9ae174bcdccb40
Yashaswini192 <voruganti.yashaswini@ust.com>
2023-03-19 22:57:44 +0530

    update basicpay for terisa to 30000 and sync with Database

--- src/main/java/com/jdbc/bl/JDBCDemo/Demo.java
+++ src/main/java/com/jdbc/bl/JDBCDemo/Demo.java
@@ -13,6 +13,7 @@ import com.mysql.cj.xdevapi.Statement;
 
 public class Demo {
 
+	//static Connection obj;
 	public static void listDrivers() {
 		Enumeration<Driver> obj1 = DriverManager.getDrivers();
 		while(obj1.hasMoreElements()) {
@@ -26,7 +27,7 @@ public class Demo {
 			PreparedStatement obj3 = obj.prepareStatement("select * from employee");
 			ResultSet result = obj3.executeQuery();
 			while(result.next()) {		
-				
+
 				System.out.println("Employee Id: "+result.getInt(1)+
 						"\nEmployee name: "+result.getString(2)+
 						"\nPhoneNumber:"+result.getString(3)+
@@ -40,7 +41,7 @@ public class Demo {
 						"\nTax:"+result.getString(10)+
 						"\nNetPay:"+result.getString(11)+
 						"\nStartDate:"+result.getString(12));
-			
+
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -49,21 +50,21 @@ public class Demo {
 		}
 
 	}
-	
+
 	public static void connectDatabase() {
-	try {
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		System.out.println("driver loaded");
-	}catch(ClassNotFoundException e) {
-		throw new IllegalStateException("cannot find the driver in the classpath!", e);
-	}
-	try {		
-		Connection obj = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root", "T@eHyung143");		    
-		System.out.println("Connection with database is succesfull");
-	} catch (SQLException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("driver loaded");
+		}catch(ClassNotFoundException e) {
+			throw new IllegalStateException("cannot find the driver in the classpath!", e);
+		}
+		try {		
+			Connection obj = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root", "T@eHyung143");		    
+			System.out.println("Connection with database is succesfull");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	public static void closeConnection(){
 
@@ -79,14 +80,39 @@ public class Demo {
 		}
 
 	}
+
+
+	public static void updateContact(){
+
+		try{
+			Connection obj = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service","root","T@eHyung143");
+			String updateSQL = "update employee_payroll set basic_pay = 30000 where name = \"Terisa\"";
+			PreparedStatement statement = obj.prepareStatement(updateSQL);
+			int update = statement.executeUpdate(updateSQL);
+
+
+			if (update > 0){
+				System.out.println("Data Successfully Updated : " + update + " Rows Updated");
+			}
+			else{
+				System.out.println("Data Not Added");
+			}
+		}
+		catch (Exception e){
+
+			System.out.println("Field Not Available");
+		}
+
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("--->Press 1 to Connect With The Database<---" +
 				"\n--->Press 2 To Get Data From Database<---" +
 				"\n-->press 3 to get list of Drivers"+
-				"\n--->Press 4 To Close The Database Connection<---");
+				"\n-->Press 4 to update data"+
+				"\n--->Press 5 To Close The Database Connection<---");
 		int userinput=sc.nextInt();
 
 		switch(userinput) {
@@ -101,6 +127,9 @@ public class Demo {
 			listDrivers();
 			break;
 		case 4:
+			updateContact();
+			break;
+		case 5:
 			closeConnection();
 			break;
 		default:
